7130be04dbc30c5c5d01b8c2acadd6229f760e42
Reanna Ishmael <rishmael@fordham.edu>
2022-07-09 22:01:15 -0400

    Add button to toggle edit mode on and off
    The app continues to display form fields even after the user has already entered their input.
    
    Add a button to toggle edit mode on and off.
    
    While in edit mode, the user can see form fields and make changes to their input.
    
    While edit mode is off, the user sees their input displayed as text.

--- src/PersonalDetails.js
+++ src/PersonalDetails.js
@@ -9,9 +9,11 @@ class PersonalDetails extends Component {
             pronouns: "",
             email: "",
             about: "",
+            isInEditMode: false,
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.toggleEditMode = this.toggleEditMode.bind(this);
     }
 
     handleInputChange(event) {
@@ -22,57 +24,75 @@ class PersonalDetails extends Component {
         })
     }
 
-    render() {
-        const { name, pronouns, email, about } = this.state;
+    toggleEditMode() {
+        this.setState((prevState) => ({
+            isInEditMode: !prevState.isInEditMode,
+        }));
+    }
 
-        return(
+    render() {
+        const { name, pronouns, email, about, isInEditMode } = this.state;
+
+        let personalDetails;
+
+        if (isInEditMode) {
+            personalDetails =                 
+            <form>
+                <label htmlFor="nameInput">
+                    Name
+                </label>
+                <input 
+                    type="text"
+                    id="nameInput"
+                    name="name"
+                    value={name}
+                    onChange={this.handleInputChange}
+                />
+                <label htmlFor="pronounsInput">
+                    Pronouns
+                </label>  
+                <input 
+                    type="text"
+                    id="pronounsInput"
+                    name="pronouns"
+                    value={pronouns}
+                    onChange={this.handleInputChange}
+                />   
+                <label htmlFor="emailInput">
+                    Email
+                </label> 
+                <input
+                    type="email"
+                    id="emailInput"
+                    name="email"
+                    value={email}
+                    onChange={this.handleInputChange}
+                />
+                <label htmlFor="aboutInput">
+                    About
+                </label> 
+                <textarea 
+                    id="aboutInput"
+                    name="about"
+                    value={about}
+                    onChange={this.handleInputChange} 
+                />
+            </form> 
+        } else {
+            personalDetails = 
             <div>
-                <h1>Personal Details</h1>
-                <form>
-                    <label htmlFor="nameInput">
-                        Name
-                    </label>
-                    <input 
-                        type="text"
-                        id="nameInput"
-                        name="name"
-                        value={name}
-                        onChange={this.handleInputChange}
-                    />
-                    <label htmlFor="pronounsInput">
-                        Pronouns
-                    </label>  
-                    <input 
-                        type="text"
-                        id="pronounsInput"
-                        name="pronouns"
-                        value={pronouns}
-                        onChange={this.handleInputChange}
-                    />   
-                    <label htmlFor="emailInput">
-                        Email
-                    </label> 
-                    <input
-                        type="email"
-                        id="emailInput"
-                        name="email"
-                        value={email}
-                        onChange={this.handleInputChange}
-                    />
-                    <label htmlFor="aboutInput">
-                        About
-                    </label> 
-                    <textarea 
-                        id="aboutInput"
-                        name="about"
-                        value={about}
-                        onChange={this.handleInputChange} 
-                    />
-                </form>    
                 <p>{name}</p>
                 <p>{pronouns}</p>
                 <p>{email}</p>          
                 <p>{about}</p>
+            </div>
+        }
+
+        return(
+            <div>
+                <h1>Personal Details</h1>  
+                <button onClick={this.toggleEditMode}>Edit</button>
+                <div>{personalDetails}</div>
             </div>
         );
     }
